4f726fe5a8683578a88835f6065895c2f3b0becf
Amanda Schuff <amandaschuff@Amandas-MacBook-Pro.local>
2016-06-02 17:29:10 -0400

    second commit

---
+++ data.js
@@ -0,0 +1,188 @@
+var albums = [
+  {
+    id: '',
+    title: "Album 1",
+    cover:'',
+    pictures: [
+        {
+        caption: '',
+        photo: '',
+      },
+      {
+        caption: '',
+        photo: '',
+      },
+      {
+        caption: '',
+        photo: ,
+      },
+      {
+        caption: '',
+        photo: ,
+      },
+      {
+        caption: '',
+        photo: ,
+      },
+      {
+        caption: '',
+        photo: ''
+      }
+    ]
+  },
+  {
+    id: '',
+    title: "Album 2",
+    cover:'',
+    pictures: [
+        {
+        caption: '',
+        photo: '',
+      },
+      {
+        caption: '',
+        photo: '',
+      },
+      {
+        caption: '',
+        photo: ,
+      },
+      {
+        caption: '',
+        photo: ,
+      },
+      {
+        caption: '',
+        photo: ,
+      },
+      {
+        caption: '',
+        photo: ''
+      }
+    ]
+  },
+  {
+    id: '',
+    title: "Album 3",
+    cover:'',
+    pictures: [
+        {
+        caption: '',
+        photo: '',
+      },
+      {
+        caption: '',
+        photo: '',
+      },
+      {
+        caption: '',
+        photo: ,
+      },
+      {
+        caption: '',
+        photo: ,
+      },
+      {
+        caption: '',
+        photo: ,
+      },
+      {
+        caption: '',
+        photo: ''
+      }
+    ]
+  },
+  {
+    id: '',
+    title: "Album 4",
+    cover:'',
+    pictures: [
+        {
+        caption: '',
+        photo: '',
+      },
+      {
+        caption: '',
+        photo: '',
+      },
+      {
+        caption: '',
+        photo: ,
+      },
+      {
+        caption: '',
+        photo: ,
+      },
+      {
+        caption: '',
+        photo: ,
+      },
+      {
+        caption: '',
+        photo: ''
+      }
+    ]
+  },
+  {
+    id: '',
+    title: "Album 5",
+    cover:'',
+    pictures: [
+        {
+        caption: '',
+        photo: '',
+      },
+      {
+        caption: '',
+        photo: '',
+      },
+      {
+        caption: '',
+        photo: ,
+      },
+      {
+        caption: '',
+        photo: ,
+      },
+      {
+        caption: '',
+        photo: ,
+      },
+      {
+        caption: '',
+        photo: ''
+      }
+    ]
+  },
+  {
+    id: '',
+    title: "Album 6",
+    cover:'',
+    pictures: [
+        {
+        caption: '',
+        photo: '',
+      },
+      {
+        caption: '',
+        photo: '',
+      },
+      {
+        caption: '',
+        photo: ,
+      },
+      {
+        caption: '',
+        photo: ,
+      },
+      {
+        caption: '',
+        photo: ,
+      },
+      {
+        caption: '',
+        photo: ''
+      }
+    ]
+  },
+];
