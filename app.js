27a13c0d54cb630a6abb8991d05e332165e5fb16
Amanda Schuff <amandaschuff@Amandas-MacBook-Pro.local>
2016-06-05 17:38:07 -0400

    sidebar stopped working. styled the pages more. all three pages show up but there's no enlarged photo

--- app.js
+++ app.js
@@ -1,70 +1,89 @@
 $(document).ready(function (){
 // puts the albums on the page
-  var albumsString = '';
-  albums.forEach(function (item, idx, arr){
-    albumsString += `<div class="album" data-id="${item.id}">
-                    <img src="${item.cover}" alt="">
-                    <h3>${item.title}</h3>
-                    </div>`;
-  })
-    $('.albums').html(albumsString);
+var albumsString = '';
+albums.forEach(function (item, idx, arr){
+  albumsString += `<div class="album" data-id="${item.id}">
+                  <img src="${item.cover}" alt="">
+                  <h3>${item.title}</h3>
+                  </div>`;
+})
+  $('.albums').html(albumsString);
 
 // when the album is clicked, the pictures are displayed and the album is hidden
-    $('.albums').on('click', '.album', function(event) {
-        event.preventDefault();
-        console.log($(this).data('id'));
-        var albumId = $(this).data('id');
-        var chosenAlbum = albums.filter(function (item, idx, arr){
-          return item.id === albumId;
-      })
-      var photosString = '';
-        chosenAlbum[0].pictures.forEach(function(item, idx, arr){
-          photosString += `<div class="photo">
-                          <img src="${item.photo}" alt="">
-                          <h3>${item.caption}</h3>
-                          </div>`
-      });
-      $('.albumThumbnails').html(photosString);
-      $('.albumThumbnails').addClass('active');
-      $('.albums').hide();
-    })
-
-// creates the list items
-    var places = albums.map(function(item, idx, arr){
-      return "<li>" + item.title + "</li>"
-    })
-  // lists the list items and puts them in the sidebar as part of the html
-      places.forEach(function(item, idx,arr){
-        return $('.sidebar').html(places)
-      })
-  //on the click of the list item, it takes you to the correct pictures in the album
-    $('li').on('click', function (event) {
+  $('.albums').on('click', '.album', function(event) {
       event.preventDefault();
-      var clickedListItem = $(this).text();
-      console.log($(this).text());
-
+      console.log($(this).data('id'));
+      var albumId = $(this).data('id');
       var chosenAlbum = albums.filter(function (item, idx, arr){
-        return item.title === clickedListItem;
-      })
+        return item.id === albumId;
+    })
     var photosString = '';
       chosenAlbum[0].pictures.forEach(function(item, idx, arr){
         photosString += `<div class="photo">
-                        <h3>${item.caption}</h3>
                         <img src="${item.photo}" alt="">
+                        <h3>${item.caption}</h3>
                         </div>`
     });
     $('.albumThumbnails').html(photosString);
     $('.albumThumbnails').addClass('active');
-    $('.albums').hide()
-    })
+    $('.albums').hide();
+  })
 
-    $('.photo').on('click',function (event) {
-      console.log(this);
-      $(this).siblings().hide();
-      // $(this).css('background-color','red');
+// creates the list items
+  var places = albums.map(function (item, idx, arr){
+    return "<li>" + item.title + "</li>"
+  })
+// lists the list items and puts them in the sidebar as part of the html
+    places.forEach(function(item){
+      return $('.sidebar').html(places)
+    })
+//on the click of the list item, it takes you to the correct pictures in the album
+$('li').on('click', function (event) {
+       event.preventDefault();
+       var clickedListItem = $(this).text();
+       console.log($(this).text());
+
+       var chosenAlbum = albums.filter(function (item, idx, arr){
+         return item.title === clickedListItem;
+       })
+     var photosString = '';
+       chosenAlbum[0].pictures.forEach(function(item, idx, arr){
+         photosString += `<div class="photo">
+                         <h3>${item.caption}</h3>
+                         <img src="${item.photo}" alt="">
+                         </div>`
+     });
+     $('.albumThumbnails').html(photosString);
+     $('.albumThumbnails').addClass('active');
+     $('.albums').hide()
+     })
+
+     $('.photo').on('click',function (event) {
+       console.log(this);
+       $(this).siblings().hide();
+       // $(this).css('background-color','red');
+ });
 });
+
 // making the picture big
 
+// $('.photo').on('click', function(event) {
+//     event.preventDefault();
+//     console.log($(this).data('id'));
+//
+//     var chosenPhoto = albums.filter(function (item, idx, arr){
+//       return
+//       $(this).find('img').attr('src');
+//         $('.photoZoom').attr('src',chosenPhoto);
+//         $(this).siblings().hide();
+//
+//   $('.photoZoom').html(photosString)
+//   $('.photoZoom').addClass('active');
+//   $(this).siblings().hide();
+//   $('.sidebar').hide();
+//
+// })
+// })
 var chosenBigPhoto =
   albums.filter(function(item, idx, arr){
     return item.photo
@@ -75,7 +94,7 @@ var chosenBigPhoto =
   // })
 $('.albumThumbnails').on('click', '.photo', function(event) {
     event.preventDefault();
-    console.log($(this).data('id'));
+    // console.log($(this).data('id'));
     var photoId = $(this).data('id');
     var chosenPhoto = albums.filter(function (item, idx, arr){
       return item.photo === photoId;
@@ -86,10 +105,11 @@ $('.albumThumbnails').on('click', '.photo', function(event) {
                       <img src="${item.photo}" alt="">
                       <h3>${item.caption}</h3>
                       </div>`
+
   });
   $('.photoZoom').html(photosBig);
   $('.photoZoom').addClass('active');
-  $('.albumThumbnails').hide();
+  $('.photo').siblings().hide();
   $('.sidebar').hide();
   $('.albums').hide();
 
@@ -97,9 +117,8 @@ $('.albumThumbnails').on('click', '.photo', function(event) {
 
 // header takes you home
   $('.header').on('click', function (item, idx, arr){
-    $('.albums').removeClass('hidden');
-    $('.albumThumbnails').addClass('hidden');
+    $('.albums').addClass('active');
+    $('.albumThumbnails').siblings().hide();
     $('.sidebar').addClass('active')
+    $('.photoZoom').addClass('hidden');
   })
-
-})
