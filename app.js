1e75e1515c427ee48b7d4067a613efe5984804a7
Amanda Schuff <amandaschuff@Amandas-MacBook-Pro.local>
2016-06-06 09:42:07 -0400

    list and home button works

--- app.js
+++ app.js
@@ -38,10 +38,11 @@ albums.forEach(function (item, idx, arr){
       return $('.sidebar').html(places)
     })
 //on the click of the list item, it takes you to the correct pictures in the album
-$('li').on('click', function (event) {
+$('.sidebar').on('click', 'li', function (event) {
+    console.log("Hey there" );
+
        event.preventDefault();
        var clickedListItem = $(this).text();
-      //  console.log($(this).text());
 
        var chosenAlbum = albums.filter(function (item, idx, arr){
          return item.id === clickedListItem;
@@ -87,34 +88,27 @@ $('.albumThumbnails').on('click', '.photo', function(event) {
   $(this).siblings().hide();
   $('.sidebar').hide();
   $('.albums').hide();
-  $('.button').hide();
 
 })
 });
-// header takes you home - doesn't work yet
-//   $('.header').on('click', function (event){
-//     event.preventDefault();
-//     console.log($(this));
-//     $('.albums').addClass('active');
-//     // $('.albumThumbnails').addClass('hidden'));
-//     $('.sidebar').addClass('active')
-// });
 
-// back to home button - doesn't work yet
-// $('input').on('click', function (event){
-//   event.preventDefault();
-//   console.log($(this).text())
-//   var albumsString = '';
-//   albums.forEach(function (item, idx, arr){
-//     albumsString += `<div class="album" data-id="${item.id}">
-//                     <img src="${item.cover}" alt="">
-//                     <h3>${item.title}</h3>
-//                     </div>`;
-//   })
-//   $('.albums').html(albumsString);
-//   $('.albums').removeClass('hidden');
-//   $('albumThumbnails').addClass('hidden');
-//
-// })
+// back to home button
+$('input').on('click', function (event){
+  event.preventDefault();
+  console.log("hey!")
+  var albumsString = '';
+  albums.forEach(function (item, idx, arr){
+    albumsString += `<div class="album" data-id="${item.id}">
+                    <img src="${item.cover}" alt="">
+                    <h3>${item.title}</h3>
+                    </div>`;
+
+  })
+  console.log("album String", albumsString);
+  $('.albums').html(albumsString);
+  $('.albums').removeClass('hidden').show();
+  $('albumThumbnails').addClass('hidden');
+
+})
 
 // })
